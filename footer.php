6dc9ed4f4766148363a4139c81bd0e24fa8e7325
Jonas Landhage <landhage@gmail.com>
2024-10-01 13:02:43 +0200

    First iteration
    First iteration of the website, focused on html and css. 
    
    Second iteration will focus on improving code structure and overall design.

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer name="main_footer">
+    <p>
+        Copyright 2024 Created and maintained by Jonas Landhage
+        <span>
+            <a href="mailto:landhage@gmail.com">Contact me</a>
+        </span>
+           
+    </p>
+</footer>
+</body>
+</html>
